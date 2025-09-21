b8de8e7ebbdb6affa97c2ec17e7452d485fa8625
eduardo-lamounier <a2025952788@teiacoltec.org>
2025-09-21 17:14:55 -0300

    Mudança de nome de variáveis

--- LeetCode/3/set.py
+++ LeetCode/3/set.py
@@ -9,15 +9,15 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         longest = 0
 
-        curr_chars = set()
+        seen = set()
         l, r = 0, 0
         while r < len(s):
-            if not s[r] in curr_chars:
-                curr_chars.add(s[r])
+            if not s[r] in seen:
+                seen.add(s[r])
                 longest = max(longest, r - l + 1)
                 r += 1
             else:
-                curr_chars.remove(s[l])
+                seen.remove(s[l])
                 l += 1
 
         return longest
