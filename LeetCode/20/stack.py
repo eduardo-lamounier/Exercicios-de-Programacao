e06e52aab55f061f31dcc0272cba88019791ddc3
eduardo-lamounier <a2025952788@teiacoltec.org>
2025-09-20 23:44:34 -0300

    20. Valid Parentheses

---
+++ LeetCode/20/stack.py
@@ -0,0 +1,32 @@
+'''
+20. Validid Parentheses - Easy
+|https://leetcode.com/problems/valid-parentheses/
+Topics:
+|String, Stack
+'''
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # Fechamento de parênteses e suas respectivas aberturas
+        brackets = {
+            ')' : '(',
+            ']' : '[',
+            '}' : '{'
+        }
+
+        stack = [] # Armazena os parênteses para serem fechados
+
+        for ch in s:
+            if not ch in brackets:
+                # Se o caractere for uma abertura de parêntese, é adicionado na
+                # stack:
+                stack.append(ch)
+                continue
+
+            # Caso o caractere seja o fechamento de um parêntese:
+            if not stack or stack.pop() != brackets[ch]:
+                # Fechamento de um parêntese que não foi aberto ou fechamento 
+                # do parêntese errado, string inválida:
+                return False
+        
+        return not stack # Se restarem parênteses não fechados, string inválida
